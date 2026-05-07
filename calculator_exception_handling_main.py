f53aa90d1073dd563839b21e6d29270ed88c0952
Tres <encantotresinigo@gmail.com>
2026-05-07 22:47:49 +0800

    Added a try except function for the: conversion of the elapsed time string into minutes which is the total_minutes variable, the pace calculator which is the pace variable, and the display of the format with the minutes_pace and seconds_pace variable. Added a ValueError and an Exception for the formats and time formats.

--- calculator_exception_handling_main.py
+++ calculator_exception_handling_main.py
@@ -38,3 +38,20 @@ def main():
             if run_distance in calculator.race_distances:
                 name, distance =  calculator.race_distances[run_distance]
                 elapsed_time = input("Enter your time for {name} (HH:MM:SS, for example: 1:59:30): ")
+
+                try:
+                    total_minutes = calculator.minutes_conversion(elapsed_time)
+
+                    pace = calculator.calculate_pace(distance, total_minutes)
+
+                    minute_pace = int(pace)
+                    seconds_pace = int((pace - minute_pace) * 60)
+                    print(f"\n-- Result: Your pace for the {name} was {minute_pace}:{seconds_pace:02d} min/KM")
+                
+                except ValueError as error_1:
+                    print(f"Format Error: {error_1}")
+                except Exception:
+                    print("An error occured. Please check your time format.")
+        
+        else:
+            print("Invalid distance selection.")
