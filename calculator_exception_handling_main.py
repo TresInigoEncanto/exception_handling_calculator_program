aed940cfea6eefc37390eeef1f268776a95aad9b
Tres <encantotresinigo@gmail.com>
2026-05-07 20:51:04 +0800

    Imported the calculator program from the functionality file. Added a prompt function for the user input in the calculator.

--- calculator_exception_handling_main.py
+++ calculator_exception_handling_main.py
@@ -1,1 +1,9 @@
-print("Calculator Exception Handling Program")
+from calculator_exception_handling_functionality import CalculatorProgram
+
+def user_input(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Error! Invalid input. Please enter a number.")
+
