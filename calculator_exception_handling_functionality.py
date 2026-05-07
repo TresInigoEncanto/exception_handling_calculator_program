59309b4135bf7d46e6f8f0c3a6b202a7263e5edf
Tres <encantotresinigo@gmail.com>
2026-05-07 20:02:18 +0800

    Created the main program and added a class for the distances in my pace calculator option that I will add with my calculator program.

--- calculator_exception_handling_functionality.py
+++ calculator_exception_handling_functionality.py
@@ -1,1 +1,6 @@
-print("Calculator Exception Handling Functionality")
+class CalculatorProgram:
+    def __init__(self):
+        self.race_distances = {
+            "1": ("3km", 3.0), "2": ("5km", 5.0), "3": ("10km", 10.0),
+            "4": ("16km", 16.0), "5": ("Half-Marathon", 21.0975), "6": ("Marathon", 42.195)
+        }
