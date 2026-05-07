74f7b5bcfbf8a8030dd523a133e7b67b736f923a
Tres <encantotresinigo@gmail.com>
2026-05-07 20:17:25 +0800

    Added a class where the total elapsed running time would be converted into minutes to be able to calculate the pace of the user. Used a time_str package to convert the user input into datetime.

--- calculator_exception_handling_functionality.py
+++ calculator_exception_handling_functionality.py
@@ -15,3 +15,13 @@ class CalculatorProgram:
         if y == 0:
             raise ZeroDivisionError("Syntax Error! Cannot divide by zero.")
         return x / y
+    
+    def minutes_conversion(self, time_str):
+        "Converts your HH:MM:SS elapsed time string to total minutes"
+        total_elapsed_time = time_str.split(':')
+        if len(total_elapsed_time) != 3:
+            raise ValueError("Time must be in HH:MM:SS (Hours:Minutes:Seconds) format (e.g, 1:59:30).")
+        
+        hours = int(total_elapsed_time[0])
+        minutes = int(total_elapsed_time[1])
+        seconds = int(total_elapsed_time[2])
