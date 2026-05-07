c132630c1dd97131a59cfc281164c06589471ec7
Tres <encantotresinigo@gmail.com>
2026-05-07 20:07:04 +0800

    Added the four operations that will be used in the calculator.

--- calculator_exception_handling_functionality.py
+++ calculator_exception_handling_functionality.py
@@ -4,3 +4,14 @@ class CalculatorProgram:
             "1": ("3km", 3.0), "2": ("5km", 5.0), "3": ("10km", 10.0),
             "4": ("16km", 16.0), "5": ("Half-Marathon", 21.0975), "6": ("Marathon", 42.195)
         }
+    
+    def addition(self, x, y): return x + y
+    
+    def subtraction(self, x, y): return x - y
+
+    def multiplication(self, x, y): return x * y
+    
+    def division(self, x, y):
+        if y == 0:
+            raise ZeroDivisionError("Syntax Error! Cannot divide by zero.")
+        return x / y
