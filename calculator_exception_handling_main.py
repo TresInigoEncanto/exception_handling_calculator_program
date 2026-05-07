7397c00f8d16edbb650aedd357d2c3060f6b8273
Tres <encantotresinigo@gmail.com>
2026-05-07 22:18:43 +0800

    Added the main function of the calculator with the desired operations included (1-5).

--- calculator_exception_handling_main.py
+++ calculator_exception_handling_main.py
@@ -7,3 +7,10 @@ def user_input(prompt):
         except ValueError:
             print("Error! Invalid input. Please enter a number.")
 
+def main():
+    calculator = CalculatorProgram
+
+    while True:
+        print("\n--- Calculator and Pace Finder ---")
+        print("1. Addition | 2. Subtraction | 3. Multiplication | 4. Division | 5. Running Pace Calculator")
+        user_choice = input("Enter your desired operation (1-5): ")
